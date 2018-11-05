51798e6605e06b18a7933c4f353c84db8daa8c3d
37426 <374269627@qq.com>
2018-11-05 20:37:11 +0800

    把本地工程添加到git

---
+++ Test.java
@@ -0,0 +1,12 @@
+package cn.itcast.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test git project");
+        System.out.println("test git project");
+        System.out.println("test git project");
+    }
+    public void test(){
+        System.out.println("修改...");
+    }
+}
